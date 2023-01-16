77cb3eef8d3ace72e962e1d4b1fe3092ca340ad3
itzBi <berkay.karakurt01@gmail.com>
2023-01-16 14:15:05 +0300

    Create Ödev.py
    kütüphane oluşturduk, devamı gelecek

---
+++ Ödev.py
@@ -0,0 +1,13 @@
+import pygame
+
+# Pygame Çalıştır Komutu
+pygame.init()
+
+# Pencere Boyutu Ayarlama
+window_size = (800, 600)
+
+# Pencere Oluşturma
+screen = pygame.display.set_mode(window_size)
+
+# Pencere Başlığı Oluşturma
+pygame.display.set_caption("Dişli Çark")
