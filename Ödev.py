968b9cb5623184109307a54d705c742f0262cdde
DmitriiUlu <dmitriy.zhelobnev@gmail.com>
2023-01-18 19:54:21 +0300

    Son güncellenme

--- Ödev.py
+++ Ödev.py
@@ -1,132 +1,99 @@
 import pygame
+import math
+import sys
 
-# Pygame Çalıştır Komutu
+# Initialize Pygame
 pygame.init()
+00
 
-# Pencere Boyutu Ayarlama
-window_size = (800, 600)
+# Set the window size
+window_size = (400, 400)
 
-# Pencere Oluşturma
+# Create the window
 screen = pygame.display.set_mode(window_size)
 
-# Pencere Başlığı Oluşturma
-pygame.display.set_caption("Drag and Drop Circles")
+# Set the title of the window
+pygame.display.set_caption("Spur Gears")
 
-# Çark Yarıçaplarını Ayarlama
-radius1 = 20
-radius2 = 40
-radius3 = 60
-radius4 = 80
-radius5 = 100
+# Set the default font and font size
+font = pygame.font.Font(None, 24)
 
-# Çark Başlangıç konumlarını Ayarlama
-pos1 = (radius1, window_size[1] - radius1)
-pos2 = (2 * radius1 + radius2, window_size[1] - radius2)
-pos3 = (3 * radius1 + 2 * radius2 + radius3, window_size[1] - radius3)
-pos4 = (4 * radius1 + 2 * radius2 + 2 * radius3 + radius4, window_size[1] - radius4)
-pos5 = (5 * radius1 + 2 * radius2 + 2 * radius3 + 2 * radius4 + radius5, window_size[1] - radius5)
+# Set the center of the window
+center1 = (window_size[0] / 2, window_size[1] / 2)
+center2 = (window_size[0]/ 4 , window_size[1]/ 4 )
 
-# Çark Renklerini Ayarlama
-color1 = (255, 0, 0)
-color2 = (0, 255, 0)
-color3 = (0, 0, 255)
-color4 = (255, 255, 0)
-color5 = (0, 255, 255)
+# Set the radius of the gears
+radius1 = 100
+radius2 = 50
 
-# Dairelerin sürükleme durumunu ayarlama
-drag1 = False
-drag2 = False
-drag3 = False
-drag4 = False
-drag5 = False
+# Set the default number of teeth on the gears
+num_teeth1 = 10
+num_teeth2 = 20
 
-#Oyun Döngüsü Çalıştırma
+# Set the rotation speed of the gears (in degrees per frame)
+rotation_speed = 1
+
+# Set the default gear ratio
+gear_ratio = num_teeth1 / num_teeth2
+
+# Set the default angle of rotation for the gears
+angle1 = 0
+angle2 = 0
+
+# Set the default color of the gears
+color = (0, 0, 0)
+
+# Set the default running state of the game
 running = True
+
+# Main game loop
 while running:
-    # Olayları İşleme
+    # Check for quit events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            # Farenin herhangi bir dairenin üzerinde olup olmadığını kontrol etme
-            if (pos1[0] - pygame.mouse.get_pos()[0]) * 2 + (pos1[1] - pygame.mouse.get_pos()[1]) * 2 <= radius1 ** 2:
-                drag1 = True
-            elif (pos2[0] - pygame.mouse.get_pos()[0]) ** 2 + (
-                    pos2[1] - pygame.mouse.get_pos()[1]) * 2 <= radius2 * 2:
-                drag2 = True
-            elif (pos3[0] - pygame.mouse.get_pos()[0]) ** 2 + (
-                    pos3[1] - pygame.mouse.get_pos()[1]) * 2 <= radius3 * 2:
-                drag3 = True
-            elif (pos4[0] - pygame.mouse.get_pos()[0]) ** 2 + (
-                    pos4[1] - pygame.mouse.get_pos()[1]) * 2 <= radius4 * 2:
-                drag4 = True
-            elif (pos5[0] - pygame.mouse.get_pos()[0]) ** 2 + (
-                    pos5[1] - pygame.mouse.get_pos()[1]) * 2 <= radius5 * 2:
-                drag5 = True
-        elif event.type == pygame.MOUSEBUTTONUP:
-            # Sürükleme durumunu sıfırlama
-            drag1 = False
-            drag2 = False
-            drag3 = False
-            drag4 = False
-            drag5 = False
+            sys.exit()
 
-        # Sürükleniyorlarsa dairelerin konumunu güncelleme
-        if drag1:
-            new_pos = pygame.mouse.get_pos()
-            dist2 = (new_pos[0] - pos2[0]) * 2 + (new_pos[1] - pos2[1]) * 2
-            dist3 = (new_pos[0] - pos3[0]) * 2 + (new_pos[1] - pos3[1]) * 2
-            dist4 = (new_pos[0] - pos4[0]) * 2 + (new_pos[1] - pos4[1]) * 2
-            dist5 = (new_pos[0] - pos5[0]) * 2 + (new_pos[1] - pos5[1]) * 2
-            if dist2 > (radius1 + radius2) * 2 and dist3 > (radius1 + radius3) * 2 and dist4 > (
-                    radius1 + radius4) * 2 and dist5 > (radius1 + radius5) * 2:
-                pos1 = new_pos
-        if drag2:
-            new_pos = pygame.mouse.get_pos()
-            dist1 = (new_pos[0] - pos1[0]) * 2 + (new_pos[1] - pos1[1]) * 2
-            dist3 = (new_pos[0] - pos3[0]) * 2 + (new_pos[1] - pos3[1]) * 2
-            dist4 = (new_pos[0] - pos4[0]) * 2 + (new_pos[1] - pos4[1]) * 2
-            dist5 = (new_pos[0] - pos5[0]) * 2 + (new_pos[1] - pos5[1]) * 2
-            if dist1 > (radius1 + radius2) * 2 and dist3 > (radius2 + radius3) * 2 and dist4 > (
-                    radius2 + radius4) * 2 and dist5 > (radius2 + radius5) * 2:
-                pos2 = new_pos
-        if drag3:
-            new_pos = pygame.mouse.get_pos()
-            dist1 = (new_pos[0] - pos1[0]) * 2 + (new_pos[1] - pos1[1]) * 2
-            dist2 = (new_pos[0] - pos2[0]) * 2 + (new_pos[1] - pos2[1]) * 2
-            dist4 = (new_pos[0] - pos4[0]) * 2 + (new_pos[1] - pos4[1]) * 2
-            dist5 = (new_pos[0] - pos5[0]) * 2 + (new_pos[1] - pos5[1]) * 2
-            if dist1 > (radius1 + radius3) * 2 and dist2 > (radius2 + radius3) * 2 and dist4 > (
-                    radius3 + radius4) * 2 and dist5 > (radius3 + radius5) * 2:
-                pos3 = new_pos
-        if drag4:
-            new_pos = pygame.mouse.get_pos()
-            dist1 = (new_pos[0] - pos1[0]) * 2 + (new_pos[1] - pos1[1]) * 2
-            dist2 = (new_pos[0] - pos2[0]) * 2 + (new_pos[1] - pos2[1]) * 2
-            dist3 = (new_pos[0] - pos3[0]) * 2 + (new_pos[1] - pos3[1]) * 2
-            dist5 = (new_pos[0] - pos5[0]) * 2 + (new_pos[1] - pos5[1]) * 2
-            if dist1 > (radius1 + radius4) * 2 and dist2 > (radius2 + radius4) * 2 and dist3 > (
-                    radius3 + radius4) * 2 and dist5 > (radius4 + radius5) * 2:
-                pos4 = new_pos
-        if drag5:
-            new_pos = pygame.mouse.get_pos()
-            dist1 = (new_pos[0] - pos1[0]) * 2 + (new_pos[1] - pos1[1]) * 2
-            dist2 = (new_pos[0] - pos2[0]) * 2 + (new_pos[1] - pos2[1]) * 2
-            dist4 = (new_pos[0] - pos4[0]) * 2 + (new_pos[1] - pos4[1]) * 2
-            dist3 = (new_pos[0] - pos3[0]) * 2 + (new_pos[1] - pos3[1]) * 2
-            if dist1 > (radius1 + radius5) * 2 and dist2 > (radius2 + radius5) * 2 and dist4 > (
-                    radius5 + radius4) * 2 and dist3 > (radius3 + radius5) * 2:
-                pos5 = new_pos
+    # Update the angle of rotation for the gears
+    angle1 += rotation_speed
+    angle2 += rotation_speed * gear_ratio
 
     # Clear the screen
     screen.fill((255, 255, 255))
 
-    # Draw the circles
-    pygame.draw.circle(screen, color1, pos1, radius1)
-    pygame.draw.circle(screen, color2, pos2, radius2)
-    pygame.draw.circle(screen, color3, pos3, radius3)
-    pygame.draw.circle(screen, color4, pos4, radius4)
-    pygame.draw.circle(screen, color5, pos5, radius5)
+    # Draw the first gear
+    for i in range(num_teeth1):
+        tooth_angle = 2 * math.pi * i / num_teeth1 + math.radians(angle1)
+        x = center1[0] + radius1 * math.cos(tooth_angle)
+        y = center1[1] + radius1 * math.sin(tooth_angle)
+        point_list = [(center1[0], center1[1]), (x, y)]
+        pygame.draw.lines(screen, color, True, point_list, 3)
 
-    # Update the display
-    pygame.display.flip()
+    # Draw the second gear
+    for i in range(num_teeth2):
+        tooth_angle = 2 * math.pi * i / num_teeth2 + math.radians(angle2)
+        x = center2[0] + radius2 * math.cos(tooth_angle)
+        y = center2[1] + radius2 * math.sin(tooth_angle)
+        point_list = [(center2[0], center2[1]), (x, y)]
+        pygame.draw.lines(screen, color, True, point_list, 3)
+
+        # Check for keypresses
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            num_teeth1 += 1
+            gear_ratio = num_teeth1 / num_teeth2
+        if keys[pygame.K_DOWN]:
+            num_teeth1 -= 1
+            gear_ratio = num_teeth1 / num_teeth2
+
+
+        # Display the gear ratio
+        text = font.render("Gear Ratio: {:.2f}".format(gear_ratio), True, (0, 0, 0))
+        screen.blit(text, (10, 10))
+
+        # Update the display
+        pygame.display.flip()
+
+    # Quit Pygame
+
+    
